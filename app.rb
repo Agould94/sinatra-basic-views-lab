7e750f03d0824d832e8c9223eff11a04333e31a1
Alexander Gould <alexander.gould94@gmail.com>
2021-04-27 14:28:29 -0700

    commit tree 58410a933a657e4d734b2fd94f4343a66450003c

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    set :views, settings.root + '/4:26-4:30/sinatra-basic-views-lab/views' 
 
+    get '/' do
+        erb :index
+    end
 
 end
